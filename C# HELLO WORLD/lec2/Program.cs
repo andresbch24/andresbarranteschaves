a9cf7ba3d5a0914251a563dc1b9aeb3539fd4eee
andresbch24 <Ma>
2016-01-18 21:22:37 +0100

    Version 1.0
    Console - Hello World

---
+++ C# HELLO WORLD/lec2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" *** ANDRÉS BARRANTES CHAVES *** ");
+            Console.ReadKey(); 
+
+        }
+    }
+}
